cd88200cfdc4f9477de7d92bbeed9300b2947077
Andrecorreadasilva1 <105030802+Andrecorreadasilva1@users.noreply.github.com>
2023-03-30 17:38:36 -0300

    teste

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Lab Pipeline DevOps 9ASO G41 - versão final")
+        self.assertEqual(self.result.data.decode('utf-8'), "Lab Pipeline DevOps 9ASO G41 - versão final!")
